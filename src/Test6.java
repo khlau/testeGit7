2044645433cb4c288bc1a5db1b300fac3f4c8319
khlau <khlau@wdcigroup.net>
2011-05-24 03:55:26 -0700

    Edited src/Test6.java via GitHub

--- src/Test6.java
+++ src/Test6.java
@@ -1,4 +1,3 @@
-
 public class Test6 {
 
 	/**
@@ -6,7 +5,7 @@ public class Test6 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+            String test="testing";
 	}
 
 }
